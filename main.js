fac3499c31df15c1aa3103b63d970a9a7704ddd3
JorgeArmandoSB <jorgesilvacreativo@gmail.com>
2022-09-13 20:14:12 -0500

    libreria matematica trunc

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Math.trunc()
+// Math.trunc() 
 // Math.max()
 // Math.min()
 // Math.pow()
@@ -7,3 +7,14 @@
 // Math.floor()
 // Math.ceil()
 
+// Math.trunc() Remueve el decimal de un número, convirtiéndolo en un entero 
+
+
+let decimal = Number(prompt(`Digita un numero decima`,2.5));
+
+
+function entero(d) {
+   return  Math.trunc(d); 
+}
+
+document.querySelector('h1').innerHTML = `numero entero es: ${entero(decimal)}`;
