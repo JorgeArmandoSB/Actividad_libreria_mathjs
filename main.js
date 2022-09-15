7b85613e6166c9a5be23bb39b66b2cbf22166958
JorgeArmandoSB <jorgesilvacreativo@gmail.com>
2022-09-15 18:19:32 -0500

    libreria math random y floor

--- main.js
+++ main.js
@@ -7,14 +7,14 @@
 // Math.floor()
 // Math.ceil()
 
-// Math.random() da un numero random de 0 a 1 en con decimales no incluye el 1, nos sirve para optener numeros aleatorios para un bingo
+// Math.random() da un numero random de 0 a 1 es con decimales no incluye el 1, nos sirve para optener numeros aleatorios para un bingo junto con math floor que nos vuelve el numero entero aproximando el decimal al numero menor mas cercano a el.
 
 let max = 100;
 let min = 1;
 
 let = numeroAleatorio = Math.floor(Math.random() * (max-min) + min);
 
-console.log(numeroAleatorio);
+
 
 
 
