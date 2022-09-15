2a5e85a7ec2e81e7d22821ef979548789ff1160e
JorgeArmandoSB <jorgesilvacreativo@gmail.com>
2022-09-15 18:24:02 -0500

    libreria matematica round

--- main.js
+++ main.js
@@ -7,12 +7,12 @@
 // Math.floor()
 // Math.ceil()
 
-// Math.random() da un numero random de 0 a 1 es con decimales no incluye el 1, nos sirve para optener numeros aleatorios para un bingo junto con math floor que nos vuelve el numero entero aproximando el decimal al numero menor mas cercano a el.
+// Math.random() da un numero random de 0 a 1 es con decimales no incluye el 1, nos sirve para optener numeros aleatorios para un bingo junto con math round que nos vuelve el numero entero aproximando el decimal, si es 0.5 se aproxima al mayor mas cercano
 
-let max = 100;
-let min = 1;
+let max = 101;
+let min = 0;
 
-let = numeroAleatorio = Math.floor(Math.random() * (max-min) + min);
+let = numeroAleatorio = Math.round(Math.random() * (max-min) + min);
 
 
 
